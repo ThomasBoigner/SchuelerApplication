a6eab1db3fe70e9d92a02da6b391966c7cf2083c
Thomas Boigner <BOI19439@spengergasse.at>
2023-03-08 15:44:51 +0100

    Initial commit

---
+++ SchuelerBackend/src/main/java/at/spengergasse/schuelerbackend/SchuelerBackendApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.schuelerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchuelerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchuelerBackendApplication.class, args);
+    }
+
+}
---
+++ SchuelerBackend/src/test/java/at/spengergasse/schuelerbackend/SchuelerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.schuelerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchuelerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
